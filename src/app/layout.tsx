dd2001c6193c2bfeea55dfb4ad93ccbe8e114a91
156631890 <156631890@qq.com>
2026-04-09 14:47:59 +0800

    feat: switch to carbon monolith theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,7 +97,7 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <meta name="theme-color" content="#f6f4ef" />
+        <meta name="theme-color" content="#06080c" />
       </head>
       <body
         className={`${spaceGrotesk.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
